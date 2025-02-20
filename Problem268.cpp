9d76ebf11207a868a17b658478cf000b2f1438f8
sanika-bhor <bhorsanika0239@gmail.com>
2025-02-21 00:01:28 +0530

    problem 268 missing number

--- Problem268.cpp
+++ Problem268.cpp
@@ -7,25 +7,48 @@
 //     Input : nums = [ 3, 0, 1 ]
 //     Output : 2
 //     Explanation :
-//     n = 3 since there are 3 numbers,so all numbers are in the range[0, 3].2 is the missing number in the range since it does not appear in nums.
-
+//        n = 3 since there are 3 numbers,so all numbers are in the range[0, 3].2 is the missing number in the range since it does not appear in nums.
 
 //Example 2 :
 //     Input : nums = [ 0, 1 ]
-//             Output : 2
+//     Output : 2
+//     Explanation :
+//         n = 2 since there are 2 numbers, so all numbers are in the range[0, 2].2 is the missing number in the range since it does not appear in nums.
 
-//             Explanation :
-
-//     n = 2 since there are 2 numbers,
-//             so all numbers are in the range[0, 2].2 is the missing number in the range since it does not appear in nums.
-
-//             Example 3 :
-
+//Example 3 :
 //     Input : nums = [ 9, 6, 4, 2, 3, 5, 7, 0, 1 ]
+//     Output : 8
+//     Explanation :
+//          n = 9 since there are 9 numbers,so all numbers are in the range[0, 9].8 is the missing number in the range since it does not appear in nums.
 
-//             Output : 8
+#include <vector>
+#include <climits>
 
-//             Explanation :
+class Solution
+{
+public:
+    int missingNumber(std::vector<int> &nums)
+    {
+        int greatest = nums.size();
+        int smallest = 0;
 
-//     n = 9 since there are 9 numbers,
-//             so all numbers are in the range[0, 9].8 is the missing number in the range since it does not appear in nums.
+        for (int i = smallest; i <= greatest; i++)
+        {
+            bool found = false;
+            for (int j = 0; j < nums.size(); j++)
+            {
+                if (nums[j] == i)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
